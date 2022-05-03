7dd9e34bf9e19d810cd246b5d7a82fd41806d5ee
Green Sky <green@g-s.xyz>
2022-05-03 17:00:45 +0200

    refactor in externals

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@
 #include <mm/services/opengl_renderer.hpp>
 #include <mm/services/screen_director.hpp>
 #include <mm/services/imgui_s.hpp>
-#include <services/mm_tox/tox_service.hpp>
+#include <mm_tox/services/tox_service.hpp>
 
 #include <mm/services/imgui_menu_bar.hpp>
 #include <mm/services/engine_tools.hpp>
 #include <mm/services/opengl_renderer_tools.hpp>
 #include <mm/services/screen_director_tools.hpp>
-#include <services/mm_tox/tox_chat.hpp>
+#include <mm_tox/services/tox_chat.hpp>
 
 bool setup_engine(MM::Engine& engine, int argc, char** argv) {
 	return setup_engine_t<
@@ -21,7 +21,7 @@ bool setup_engine(MM::Engine& engine, int argc, char** argv) {
 		MM::Services::FilesystemService, // enabled by default
 
 		//MM::Services::SoundService,
-		MM::Services::Tox::ToxService,
+		MM::Tox::Services::ToxService,
 
 		MM::Services::OpenGLRenderer,
 
@@ -30,7 +30,7 @@ bool setup_engine(MM::Engine& engine, int argc, char** argv) {
 		MM::Services::ImGuiEngineTools,
 		MM::Services::ImGuiOpenGLRendererTools,
 		MM::Services::ImGuiScreenDirectorTools,
-		MM::Services::Tox::ToxChat,
+		MM::Tox::Services::ToxChat,
 
 		// your services here
 
